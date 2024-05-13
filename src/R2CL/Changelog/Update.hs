c1ec6fd151e7a1db4e7c1330283e19a3e95ee314
patrick brisbin <pbrisbin@gmail.com>
2024-05-13 16:39:41 -0400

    Refactor

--- src/R2CL/Changelog/Update.hs
+++ src/R2CL/Changelog/Update.hs
@@ -1,42 +1,62 @@
 module R2CL.Changelog.Update
   ( updateChangelog
+
+    -- * Exported for testing
+  , InvalidPragmas (..)
   , updateChangelogLines
   ) where
 
 import Relude
 
-import Data.List (findIndex)
+import Data.List (elemIndex)
 import Data.Text qualified as T
+import UnliftIO.Exception (throwIO)
+
+data InvalidPragmas
+  = EndWithoutBegin Int
+  | EndBeforeBegin Int Int
+  deriving stock (Show)
+
+instance Exception InvalidPragmas where
+  displayException = \case
+    EndWithoutBegin e ->
+      "END present at index "
+        <> show e
+        <> "but no BEGIN found"
+    EndBeforeBegin e b ->
+      "END present at index "
+        <> show e
+        <> " is before BEGIN found at "
+        <> show b
 
 updateChangelog :: MonadIO m => FilePath -> Text -> m ()
 updateChangelog path rendered = do
   writeFileText path
     . T.unlines
+    =<< either throwIO pure
     . updateChangelogLines rendered
     . T.lines
     . decodeUtf8
     =<< readFileBS path
 
-updateChangelogLines :: Text -> [Text] -> [Text]
-updateChangelogLines rendered = \case
-  -- End recursion
-  [] -> []
-  -- Found the start line
-  (l : ls) | l == beginPragma ->
-    -- Drop until an end line
-    case dropUntilStrictly (/= endPragma) ls of
-      -- We found an end line insert it between
-      Just after -> [beginPragma, rendered, endPragma] <> after
-      -- We found end line, insert it all here
-      Nothing -> [beginPragma, rendered, endPragma] <> ls
-  -- No start line, recurse
-  (l : ls) -> l : updateChangelogLines rendered ls
+updateChangelogLines :: Text -> [Text] -> Either InvalidPragmas [Text]
+updateChangelogLines rendered ls =
+  case (beginIndex, endIndex) of
+    (Nothing, Nothing) -> Right $ rendered' <> ls
+    (Nothing, Just e) -> Left $ EndWithoutBegin e
+    (Just b, Nothing) -> Right $ take b ls <> rendered' <> drop (b + 1) ls
+    (Just b, Just e) | e > b -> Right $ take b ls <> rendered' <> drop (e + 1) ls
+    (Just b, Just e) -> Left $ EndBeforeBegin e b
+ where
+  rendered' = [beginPragma, rendered, endPragma]
+  beginIndex = elemIndex beginPragma ls
+  endIndex = elemIndex endPragma ls
 
 beginPragma :: Text
-beginPragma = "<!-- r2cl BEGIN -->"
+beginPragma = r2clPragma "BEGIN"
 
 endPragma :: Text
-endPragma = "<!-- r2cl END -->"
+endPragma = r2clPragma "END"
 
-dropUntilStrictly :: (a -> Bool) -> [a] -> Maybe [a]
-dropUntilStrictly p as = ($ as) . drop <$> findIndex p as
+r2clPragma :: Text -> Text
+r2clPragma x = "<!-- r2cl " <> x <> " -->"
